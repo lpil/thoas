9c21cd9810a019781574457784e8645aff865b29
Sergey Prokhorov <seriy.pr@gmail.com>
2022-01-26 01:09:59 +0100

    Add property-based tests (some are failing)
    * `escape => html` sometimes fails
    * `[{K, V}]` representation of JSON objects seems to not be supported

---
+++ test/prop_thoas.erl
@@ -0,0 +1,212 @@
+-module(prop_thoas).
+
+-export([
+         json_term/0, input_term/0
+]).
+
+-include_lib("proper/include/proper.hrl").
+%% -include_lib("common_test/include/ct.hrl").
+-include_lib("stdlib/include/assert.hrl").
+
+prop_decode_never_fails() ->
+    ?FORALL(
+       {Input, DecodeOpts},
+       {oneof([
+               ?LET(JsonStruct, json_term(), thoas:encode(JsonStruct)),
+               proper_unicode:utf8(),
+               binary(),
+               list(oneof([byte(), binary()]))
+              ]),
+        decode_options()},
+       case thoas:decode(Input, DecodeOpts) of
+           {ok, _} ->
+               true;
+           {error, _} ->
+               true;
+           Other ->
+               throw({unexpected_return, Other})
+       end).
+
+prop_valid_input_always_encodable() ->
+    ?FORALL(
+       {Input, EncOpts},
+       {json_term(), encode_options()},
+       begin
+           is_binary(thoas:encode(Input, EncOpts))
+       end).
+
+prop_encode_decode_matches() ->
+    ?FORALL(
+       {Input, EncOpts},
+       {json_term(), encode_options()},
+       begin
+           Encoded = thoas:encode(Input, EncOpts),
+           ?assertEqual({ok, Input}, thoas:decode(Encoded)),
+           true
+       end).
+
+prop_input_term_encodable_decodable() ->
+    ?FORALL(
+       {Input, EncOpts},
+       {input_term(), encode_options()},
+       begin
+           Encoded = thoas:encode(Input, EncOpts),
+           ?assertMatch({ok, _}, thoas:decode(Encoded)),
+           true
+       end).
+    
+
+prop_thoas_encode_jsx_decode_match() ->
+    ?FORALL(
+       Input,
+       json_term(),
+       begin
+           Encoded = thoas:encode(Input),
+           ?assertEqual(Input, jsx:decode(Encoded)),
+           true
+       end).
+
+prop_jsx_encode_thoas_decode_match() ->
+    ?FORALL(
+       Input,
+       json_term(),
+       begin
+           Encoded = jsx:encode(Input),
+           ?assertEqual({ok, Input}, thoas:decode(Encoded)),
+           true
+       end).
+
+%% Not realy reliable
+%% prop_encode_not_slower_than_jsx() ->
+%%     ?FORALL(
+%%        Input,
+%%        ?SIZED(Size, json_term(Size + 10)),
+%%        begin
+%%            erlang:garbage_collect(),
+%%            {JsxTime, _} = timer:tc(run_n_f(fun() -> jsx:encode(Input) end, 50)),
+%%            erlang:garbage_collect(),
+%%            {ThoasTime, _} = timer:tc(run_n_f(fun() -> thoas:encode(Input) end, 50)),
+%%            ?assert(ThoasTime / JsxTime < 2,
+%%                    [{thoas_time, ThoasTime},
+%%                     {jsx_time, JsxTime}]),
+%%            true
+%%        end).
+
+%% prop_decode_not_slower_than_jsx() ->
+%%     ?FORALL(
+%%        Input,
+%%        ?SIZED(Size, json_term(Size + 10)),
+%%        begin
+%%            JsonBin = thoas:encode(Input),
+%%            erlang:garbage_collect(),
+%%            {JsxTime, _} = timer:tc(run_n_f(fun() -> jsx:decode(JsonBin) end, 50)),
+%%            erlang:garbage_collect(),
+%%            {ThoasTime, _} = timer:tc(run_n_f(fun() -> thoas:decode(JsonBin) end, 50)),
+%%            ?assert(ThoasTime / JsxTime < 2,
+%%                    [{thoas_time, ThoasTime},
+%%                     {jsx_time, JsxTime}]),
+%%            true
+%%        end).
+
+%% run_n_f(Fun, N) ->
+%%     fun() -> run_n(Fun, N) end.
+
+%% run_n(_Fun, 0) -> ok;
+%% run_n(Fun, N) -> 
+%%     Fun(),
+%%     run_n(Fun, N - 1).
+
+%% Generator
+
+encode_options() ->
+    oneof(
+      [
+       #{},
+       ?LET(Escape,
+            oneof([json, unicode, html, javascript]),
+            #{escape => Escape})
+      ]).
+
+decode_options() ->
+    oneof(
+      [
+       #{},
+       ?LET(Strings,
+            oneof([reference, copy]),
+            #{strings => Strings})
+      ]).
+-define(DIV_FACTOR, 4).
+
+%% flexible representation (accepted by encoder, but can be altered by decoder)
+input_term() ->
+  ?SIZED(Size, input_term(Size)).
+
+input_term(Size) ->
+  ?LAZY(proper_types:frequency(
+          [{40, json_term(Size)},
+           {10, ?SUCHTHAT(Atom, atom(), not lists:member(Atom, [true, false, null]))},
+           {20, j_array(Size, fun input_term/1)},
+           {10, j_object(Size, fun input_term/1)},
+           {20, proplist_object(Size)}
+          ])).
+
+proplist_object(0) ->
+    #{};                                        %[] would be confused for array
+proplist_object(Size) ->
+    proper_types:list({proper_types:oneof([j_string(), atom()]),
+                       input_term(Size div ?DIV_FACTOR)}).
+
+%% Canonical representation (returned by decoder)
+json_term() ->
+  ?SIZED(Size, json_term(Size)).
+
+
+json_term(0) ->
+  j_literal();
+json_term(Size) ->
+  ?LAZY(proper_types:frequency(
+          [{50, j_literal()},
+           {25, j_array(Size)},
+           {25, j_object(Size)}])).
+
+j_object(Size) ->
+    j_object(Size, fun json_term/1).
+j_object(0, _) ->
+  #{};
+j_object(Size, Of) ->
+  ?LET(KV,
+       proper_types:list({j_string(), Of(Size div ?DIV_FACTOR)}),
+       maps:from_list(KV)).
+
+j_array(Size) ->
+    j_array(Size, fun json_term/1).
+j_array(0, _) ->
+  [];
+j_array(Size, Of) ->
+  proper_types:list(Of(Size div ?DIV_FACTOR)).
+
+j_string() ->
+    proper_types:oneof(
+      [alphanum(),
+       proper_unicode:utf8()]).
+
+
+j_literal() ->
+    proper_types:oneof(
+      [j_string(),
+       proper_types:integer(),
+       proper_types:float(),
+       proper_types:boolean(),
+       null]).
+
+alphanum() ->
+  %% proper_unicode:utf8().
+  ?LET(Str,
+       proper_types:non_empty(alphanum_str()),
+       unicode:characters_to_binary(Str)).
+
+alphanum_str() ->
+  proper_types:list(
+    proper_types:oneof(lists:seq($a, $z) ++
+                         lists:seq($A, $Z) ++
+                         lists:seq($0, $9))).
